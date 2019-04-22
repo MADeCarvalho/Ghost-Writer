72ac84cca8dd0699bdeb5ab117f672673a0c0719
JDeCarvalho <Carvalho96v2@gmail.com>
2019-04-22 22:35:26 +0200

    Refactored code, added ability to take screenshots

--- test.py
+++ test.py
@@ -1,23 +1,60 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from resize_and_crop import resize_and_crop
+from selenium.webdriver.common.action_chains import ActionChains
+from PIL import Image
+from time import sleep
+
+_currentPage = 'https://www.reddit.com/r/AskReddit'
+_commentNum = 0
+
+def _initDriver():
+    options = Options()
+    options.add_argument("--disable-notifications")
+    driver = webdriver.Chrome( chrome_options=options)
+    driver.maximize_window()
+    driver.implicitly_wait(30)
+    return driver
 
 
-options = Options()
-options.add_argument("--disable-notifications")
+def _enterThread(driver) :
+    link = driver.find_element_by_partial_link_text("hours ago")
+    driver.get(link.get_attribute('href'))
+    _currentPage = link 
 
-driver = webdriver.Chrome( chrome_options=options)
+def _getComment(driver, index = 0) :
+    return driver.find_elements_by_xpath("//*[contains(concat(' ', @class, ' '), ' top-level ')]")[index]
 
-driver.get("https://www.reddit.com/r/AskReddit")
-link = driver.find_element_by_partial_link_text("hours ago")
-link.click()
+def _getCommentLoop(driver, index, failCount = 0):
+  comNo = index
+  try:
+    print("in try")
+    comment = _getComment(driver, comNo)
+    comment.screenshot("images\\comment{}.png".format(comNo))
+    driver.execute_script("""
+    var element = arguments[0];
+    element.parentNode.removeChild(element);
+    """, comment)
+    comNo = comNo + 1
+    _getCommentLoop(driver, comNo)
+  except Exception as e:
+    _failCount = failCount +1
+    print(e)
+    print("fail at {}".format(comNo))
+    if(_failCount > 3):
+      return
+    sleep(60)
+    _getCommentLoop(driver, comNo, failCount=_failCount)
 
-post_heading = driver.find_element_by_tag_name("h2")
-top_left = post_heading.location
-size = post_heading.size
-screenshot =driver.get_screenshot_as_file("/images/test.png")
-image = resize_and_crop("/images/test.png", (size["width"],size["height"]),"middle")
+if __name__ == "__main__":
+  driver = _initDriver()
+  driver.get(_currentPage)
+  _enterThread(driver)
+  post_heading = driver.find_element_by_tag_name("h2")
+  post_heading.screenshot('images\\title.png')
+  _getCommentLoop(driver, 0)
+  driver.close()
 
-with file('test.png', 'wb') as f:
-    f.write(region)
+
+
+
